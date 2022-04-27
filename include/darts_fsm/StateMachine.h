0a4093682f8eef8588fcd2cf1d197de825d8bc1f
SparkChen927 <516728164@qq.com>
2022-04-27 17:16:19 +0800

    Changes

--- include/darts_fsm/StateMachine.h
+++ include/darts_fsm/StateMachine.h
@@ -6,25 +6,50 @@
 //#include "StateMachine_sm.h"
 #include <ros/ros.h>
 #include <rm_msgs/DbusData.h>
+#include <effort_controllers/joint_velocity_controller.h>
+#include <effort_controllers/joint_position_controller.h>
+#include <rm_common/ros_utilities.h>
+#include <rm_msgs/ShootCmd.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <rm_common/decision/command_sender.h>
 
 class StateMachine{
 public:
   StateMachine(ros::NodeHandle &nh);
 
-  bool isAim(rm_msgs::DbusData data_dbus_);//pitch&raw
+  bool isAim(rm_msgs::DbusData data_dbus_) {
+    return false;
+  }
 
-  bool isReady(rm_msgs::DbusData data_dbus_);
+  bool isReady(rm_msgs::DbusData data_dbus_) {
+    if (data_dbus_.s_l == rm_msgs::DbusData::MID)
+      return true;
+    else
+      return false;
+  }
 
-  bool isPush(rm_msgs::DbusData data_dbus_);
+  bool isPush(rm_msgs::DbusData data_dbus_) {
+    if (data_dbus_.s_l == rm_msgs::DbusData::UP)
+      return true;
+    else
+      return false;
+  }
 
-  bool isBack(rm_msgs::DbusData data_dbus_);//回调
+  bool isBack(rm_msgs::DbusData data_dbus_) {
+    if (data_dbus_.s_l == rm_msgs::DbusData::DOWN)
+      return true;
+    else
+      return false;
+  }
 
   void initAim();
 
-  void initReady();
+  void initReady(rm_msgs::DbusData data_dbus_);
 
   void initPush();
 
   void initBack();
 
+private:
+  rm_common::ShooterCommandSender *shooter_cmd_sender_{};
 };
--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -7,34 +7,25 @@ StateMachine::StateMachine(ros::NodeHandle &nh) {
 
 }
 
-bool StateMachine::isAim(rm_msgs::DbusData data_dbus_) {
-  return false;
-}
-
-bool StateMachine::isReady(rm_msgs::DbusData data_dbus_) {
-  return false;
-}
-
-bool StateMachine::isPush(rm_msgs::DbusData data_dbus_) {
-  return false;
-}
-
-bool StateMachine::isBack(rm_msgs::DbusData data_dbus_) {
-  return false;
-}
-
 void StateMachine::initAim() {
-
+  ROS_INFO("Enter Aim");
 }
 
-void StateMachine::initReady() {
-
+void StateMachine::initReady(rm_msgs::DbusData data_dbus_) {
+  ROS_INFO("Enter Ready");
+  if (data_dbus_.s_l == rm_msgs::DbusData::UP)
+  {
+    shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::SPEED_30M_PER_SECOND);
+  }
 }
 
 void StateMachine::initPush() {
-
+  ROS_INFO("Enter Push");
+  shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::PUSH);
 }
 
 void StateMachine::initBack() {
-
+  ROS_INFO("Enter Back");
 }
+
+
