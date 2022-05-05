df4c8485b195ca45fb7dcbcd1713f2c9f9702127
SparkChen927 <516728164@qq.com>
2022-05-05 16:25:56 +0800

    To be continued.

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,5 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "darts_fsm");
   ros::NodeHandle darts_nh("~");
   robot = getParam(darts_nh, "robot_type", (std::string) "error");
-  StateMachine sm(darts_nh);
-  ros::Rate loop_rate(100);
-  while (ros::ok()) {
-    ros::Time time = ros::Time::now();
-    ros::Time begin_time = ros::Time::now();
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
   return 0;
 }
