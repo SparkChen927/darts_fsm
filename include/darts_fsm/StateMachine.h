86cc972c5277d4aa7abc8a01fbe95bfa2203fcce
SparkChen927 <516728164@qq.com>
2022-05-05 16:36:01 +0800

    Delete useless part and rewrite some functions.

--- include/darts_fsm/StateMachine.h
+++ include/darts_fsm/StateMachine.h
@@ -10,11 +10,16 @@
 #include <effort_controllers/joint_velocity_controller.h>
 #include <effort_controllers/joint_position_controller.h>
 #include <rm_common/decision/command_sender.h>
-#include <rm_common/referee/referee.h>
 
 struct Config
 {
-  double qd_15, qd_30, lf_extra_rotat_speed;
+  double qd_15, qd_30, ctrl_vel;
+};
+
+struct s
+{
+  rm_msgs::DbusData::_s_l_type s_l_;
+  rm_msgs::DbusData::_s_r_type s_r_;
 };
 
 class StateMachine{
@@ -42,38 +47,34 @@ public:
       return false;
   }
 
-  void Gimbal(rm_msgs::DbusData data_dbus_);
+  void Gimbal(const ros::Time& time, const ros::Duration& period);
 
   void initReady();
-  void Ready();
 
   void initPush();
-  void Push();
 
   void initBack();
-  void Back();
 
   void dbusCB(const rm_msgs::DbusData::ConstPtr &dbus_data) {
     dbus_ = *dbus_data;
     context_.dbusUpdate(*dbus_data);
   }
 
-  void getReady(rm_msgs::DbusData data_dbus_);
+  void commandCB(const rm_msgs::DbusData::_s_r_type &msg);
 
 protected:
   StateMachineContext context_;
 
 private:
-  rm_common::GimbalCommandSender *gimbal_cmd_sender_{};
-  rm_common::ShooterCommandSender *shooter_cmd_sender_{};
   ros::NodeHandle nh_;
-  rm_common::RefereeData data_;
   rm_msgs::DbusData dbus_;
   rm_msgs::DbusData dbus_data_;
   effort_controllers::JointPositionController ctrl_trigger_;
+  effort_controllers::JointPositionController ctrl_yaw_, ctrl_pitch_;
   effort_controllers::JointVelocityController ctrl_friction_l_, ctrl_friction_r_;
+  realtime_tools::RealtimeBuffer<s> cmd_rt_buffer_;
   ros::Subscriber dbus_sub_;
-  int ch_r_state_;
+  s s_struct_;
   Config config_{};
   int push_per_rotation_{};
 };
--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -5,38 +5,32 @@
 
 StateMachine::StateMachine(ros::NodeHandle &nh, ros::NodeHandle& controller_nh) : context_(*this) {
   config_ = {
-             .qd_15 = getParam(controller_nh, "qd_15", 0.),
-             .qd_30 = getParam(controller_nh, "qd_30", 0.),
-             .lf_extra_rotat_speed = getParam(controller_nh, "lf_extra_rotat_speed", 0.)
+      .qd_15 = getParam(controller_nh, "qd_15", 0.),
+      .qd_30 = getParam(controller_nh, "qd_30", 0.),
+      .ctrl_vel = getParam(controller_nh, "ctrl_vel", 0.),
   };
   push_per_rotation_ = getParam(controller_nh, "push_per_rotation", 0);
-  nh_ = ros::NodeHandle(nh);
-  ros::NodeHandle gimbal_nh(nh_, "gimbal");
-  gimbal_cmd_sender_ = new rm_common::GimbalCommandSender(gimbal_nh, data_);
-  ros::NodeHandle shooter_nh(nh_, "shooter");
-  shooter_cmd_sender_ = new rm_common::ShooterCommandSender(shooter_nh, data_);
   dbus_sub_ = nh_.subscribe<rm_msgs::DbusData>("/dbus_data", 10, &StateMachine::dbusCB, this);
   context_.enterStartState();
 }
 
-void StateMachine::Gimbal(rm_msgs::DbusData data_dbus_) {
-  gimbal_cmd_sender_->setRate(data_dbus_.ch_l_x,data_dbus_.ch_l_y);
+void StateMachine::Gimbal(const ros::Time& time, const ros::Duration& period) {
+  ctrl_yaw_.setCommand(config_.ctrl_vel);
+  ctrl_pitch_.setCommand(config_.ctrl_vel);
+  ctrl_yaw_.update(time, period);
+  ctrl_pitch_.update(time, period);
 }
 
 void StateMachine::initReady() {
   ROS_INFO("Enter Ready");
-}
-
-void StateMachine::Ready() {
-  getReady(dbus_data_);
-  if(ch_r_state_ == 1)
+  if(s_struct_.s_r_ == rm_msgs::DbusData::UP)
   {
-    ctrl_friction_l_.setCommand(config_.qd_15 + config_.lf_extra_rotat_speed);
+    ctrl_friction_l_.setCommand(config_.qd_15);
     ctrl_friction_r_.setCommand(-config_.qd_15);
   }
-  else if(ch_r_state_ == 2)
+  else if(s_struct_.s_r_ == rm_msgs::DbusData::MID)
   {
-    ctrl_friction_l_.setCommand(config_.qd_30 + config_.lf_extra_rotat_speed);
+    ctrl_friction_l_.setCommand(config_.qd_30);
     ctrl_friction_r_.setCommand(-config_.qd_30);
   }
   else
@@ -47,39 +41,18 @@ void StateMachine::Ready() {
 
 void StateMachine::initPush() {
   ROS_INFO("Enter Push");
-}
-
-void StateMachine::Push() {
-  if(ch_r_state_ == 1)
-  {
     ctrl_trigger_.setCommand(ctrl_trigger_.command_struct_.position_ -
                              2. * M_PI / static_cast<double>(push_per_rotation_));
-  }
-  else if(ch_r_state_ == 2)
-  {
-    ctrl_trigger_.setCommand(ctrl_trigger_.command_struct_.position_ -
-                             2. * M_PI / static_cast<double>(push_per_rotation_));
-  }
 }
 
 void StateMachine::initBack() {
   ROS_INFO("Enter Back");
-}
-
-void StateMachine::Back() {
-  shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::STOP);
   ctrl_trigger_.setCommand(-(ctrl_trigger_.command_struct_.position_ -
-                           2. * M_PI / static_cast<double>(push_per_rotation_)));
+                             2. * M_PI / static_cast<double>(push_per_rotation_)));
 }
 
-void StateMachine::getReady(rm_msgs::DbusData data_dbus_) {
-  if(data_dbus_.s_r == rm_msgs::DbusData::UP)
-  {
-    ch_r_state_ = 1;
-  }
-  else if(data_dbus_.s_r == rm_msgs::DbusData::MID)
-  {
-    ch_r_state_ = 2;
-  }
+void StateMachine::commandCB(const rm_msgs::DbusData::_s_r_type &msg) {
+  s_struct_.s_r_ = msg;
+  cmd_rt_buffer_.writeFromNonRT(s_struct_);
 }
 
