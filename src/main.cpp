a71650837654455edd53f88ce89d301bd631ad68
SparkChen927 <516728164@qq.com>
2022-05-05 13:45:17 +0800

    Create file "main.cpp".

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+//
+// Created by spark on 7/27/20.
+//
+
+#include "darts_fsm/StateMachine.h"
+
 int main(int argc, char **argv) {
-    return 0;
+  std::string robot;
+  ros::init(argc, argv, "darts_fsm");
+  ros::NodeHandle darts_nh("~");
+  robot = getParam(darts_nh, "robot_type", (std::string) "error");
+  StateMachine sm(darts_nh);
+  ros::Rate loop_rate(100);
+  while (ros::ok()) {
+    ros::Time time = ros::Time::now();
+    ros::Time begin_time = ros::Time::now();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
 }
